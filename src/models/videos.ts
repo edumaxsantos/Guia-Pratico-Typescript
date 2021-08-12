914a85e7e9bcfd3768aba54f7b1bdc2a71f5b5a6
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-08-11 23:04:56 -0300

    Added jsdoc to model

--- src/models/videos.ts
+++ src/models/videos.ts
@@ -1,5 +1,10 @@
 import { Core } from "./core";
 
+/**
+ * Model de vídeo
+ * @type url {String} url do vídeo
+ * @type duracao {String} tempo do vídeo
+ */
 export class Videos extends Core {
   url: String;
   duracao: String;
