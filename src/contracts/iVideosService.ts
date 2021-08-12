679c29e7de0dfb6cb6c943051aa1dce869fc03b4
Eduardo M. C. Santos <edumaxsantos@gmail.com>
2021-08-11 23:10:59 -0300

    Added jsdoc to contracts

--- src/contracts/iVideosService.ts
+++ src/contracts/iVideosService.ts
@@ -1,4 +1,8 @@
 import { Videos } from "../models/videos";
 import { IService } from "./iService";
 
+/**
+ * Contrato IVideosService
+ * @summary esse contrato implementa a interface IService passando a model de Videos
+ */
 export interface IVideosService extends IService<Videos> {}
